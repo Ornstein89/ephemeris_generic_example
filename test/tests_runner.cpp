0b6997ace72746c499bbb33de747fd6a00625e53
Anton Nedogarok <nk260an@gmail.com>
2023-09-09 22:37:50 +0300

    Added test template

---
+++ test/tests_runner.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+TEST(cspice_de440_test, positive)
+{
+
+}
+
+TEST(libephaccess_epm2021_test, positive)
+{
+
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
